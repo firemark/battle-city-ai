5cd95851cbf26b75625f673e3e1578002387ad9b
Firemark <marpiechula@gmail.com>
2018-08-05 01:09:39 +0200

    better test_it script

--- test_it.sh
+++ test_it.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+trap finish INT
+
+function finish() {
+    kill -9 $P1 $P2 $P3
+}
+
+echo "^C to cancel..."
+
 python -m battle_city.server&
 P1=$!
 sleep 2
@@ -9,7 +17,6 @@ P3=$!
 
 echo pids: $P1 $P2 $P3
 
-echo "enter to cancel..."
-read foobar
-kill -9 $P1 $P2 $P3 
-
+wait $P1
+wait $P2
+wait $P3
