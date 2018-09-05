79dd55467f67d270b3c114d02e95462fc8ccf1fd
Firemark <marpiechula@gmail.com>
2018-09-05 20:42:39 +0200

    add arguments to test_it soft

--- test_it.sh
+++ test_it.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
+set -e
 trap finish INT
 
 function finish() {
     kill -9 $P1 $P2 $P3
 }
 
+CMD_A="python -m battle_city.client"
+CMD_B="python -m battle_city.client"
+while getopts a:b:h option; do
+    case $option in
+        a) CMD_A=$OPTARG;;
+        b) CMD_B=$OPTARG;;
+        h) echo "cat this file"; exit;;
+    esac
+done
+
 echo "^C to cancel..."
 
 python -m battle_city.server&
 P1=$!
 sleep 2
-python -m battle_city.client > /dev/null&
+$CMD_A > /dev/null&
 P2=$!
-python -m battle_city.client > /dev/null&
+$CMD_B > /dev/null&
 P3=$!
 
 echo pids: $P1 $P2 $P3
--- test_it_tmux.sh
+++ test_it_tmux.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+CMD_A="python -m battle_city.client"
+CMD_B="python -m battle_city.client"
+while getopts a:b:h option; do
+    case $option in
+        a) CMD_A=$OPTARG;;
+        b) CMD_B=$OPTARG;;
+        h) echo "cat this file"; exit;;
+    esac
+done
+
 SESSION=$USER-BATTLECITY
 
 tmux -2 new-session -d -s $SESSION "/bin/sh"
@@ -16,11 +26,11 @@ tmux split-window -h "/bin/sh"
 
 # left window - 1st player
 tmux select-pane -t 1
-tmux send-keys "echo PLAYER 1; sleep 2 && python -m battle_city.client" C-m
+tmux send-keys "echo PLAYER 1; sleep 2 && $CMD_A" C-m
 
 # right window - 2nd player
 tmux select-pane -t 2
-tmux send-keys "echo PLAYER 2; sleep 2 && python -m battle_city.client" C-m
+tmux send-keys "echo PLAYER 2; sleep 2 && $CMD_B" C-m
 
 # return to server window
 tmux select-pane -t 0
