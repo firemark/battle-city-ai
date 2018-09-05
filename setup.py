6c72c2ed64272f5627c52ab59f2cf445bcc45e40
Firemark <marpiechula@gmail.com>
2018-09-06 00:14:32 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,17 @@ setup(
     version='0.1',
     description='whatever',
     packages=find_packages(exclude=['tests', 'docs']),
-    install_requires=['pygame'],
+    install_requires=[
+        'pygame==1.9.4',
+    ],
     include_package_data=True,
     extras_require={
-        'test': ['pytest'],
+        'test': [
+            'pytest==3.6.3',
+            'pytest-asyncio==0.9.0',
+            'pytest-cov==2.5.1',
+            'asynctest==0.12.2',
+            'coverage==4.5.1', 
+        ],
     },
 )
