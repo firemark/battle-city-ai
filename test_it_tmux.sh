6af091db902a59f10bb29d276fdcbd22a43cf0fe
Firemark <marpiechula@gmail.com>
2018-09-05 22:30:07 +0200

    better tmux

--- test_it_tmux.sh
+++ test_it_tmux.sh
@@ -11,14 +11,14 @@ while getopts a:b:h option; do
     esac
 done
 
-SESSION=$USER-BATTLECITY
+SESSION=${USER}_BATTLECITY
 
 tmux -2 new-session -d -s $SESSION "/bin/sh"
 
 # top window - server
 tmux split-window -v "/bin/sh"
 tmux select-pane -t 0
-tmux send-keys "python -m battle_city.server; read; tmux kill-server" C-m
+tmux send-keys "clear; python -m battle_city.server; read; tmux kill-session -t $SESSION" C-m
 
 # player windows
 tmux select-pane -t 1 
@@ -26,11 +26,11 @@ tmux split-window -h "/bin/sh"
 
 # left window - 1st player
 tmux select-pane -t 1
-tmux send-keys "echo PLAYER 1; sleep 2 && $CMD_A" C-m
+tmux send-keys "clear; echo PLAYER 1; sleep 2 && $CMD_A" C-m
 
 # right window - 2nd player
 tmux select-pane -t 2
-tmux send-keys "echo PLAYER 2; sleep 2 && $CMD_B" C-m
+tmux send-keys "clear; echo PLAYER 2; sleep 2 && $CMD_B" C-m
 
 # return to server window
 tmux select-pane -t 0
