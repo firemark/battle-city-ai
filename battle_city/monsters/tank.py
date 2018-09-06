523d0a50d80bf22b71b23916a033b3acfd3443bd
Firemark <marpiechula@gmail.com>
2018-09-07 01:12:13 +0200

    better set direction for tanks

--- battle_city/monsters/tank.py
+++ battle_city/monsters/tank.py
@@ -3,6 +3,8 @@ from battle_city.basic import Direction
 
 
 class Tank(Monster):
+    HOR_DIRECTIONS = {Direction.LEFT, Direction.RIGHT}
+    VER_DIRECTIONS = {Direction.UP, Direction.DOWN}
     is_shot: bool = False
     SIZE = 32
 
@@ -13,12 +15,15 @@ class Tank(Monster):
         self.is_shot = True
 
     def set_direction(self, direction):
+        old_direction = self.direction
         super().set_direction(direction)
 
         # round after change direction like in original battle city.
         # with rounding is more easy to move tank beetwen walls
         part_size = self.SIZE / 2
-        if direction in {Direction.UP, Direction.DOWN}:
+        if direction in self.VER_DIRECTIONS and old_direction not in self.VER_DIRECTIONS:
             self.position.x = round(self.position.x / part_size) * part_size
-        else:
+            return
+
+        if direction in self.HOR_DIRECTIONS and old_direction not in self.HOR_DIRECTIONS:
             self.position.y = round(self.position.y / part_size) * part_size
